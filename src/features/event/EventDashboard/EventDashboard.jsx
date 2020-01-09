4bace94e89a64a578f3cc06b9c15a014e4d0da4f
Gili Sinai <gilisinai2016@gmail.com>
2020-01-09 15:22:46 +0200

    add form submit

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Grid, Button } from 'semantic-ui-react'
 import EventList from '../EventList/EventList'
 import EventForm from '../EventForm/EventForm'
+import cuid from 'cuid'
 
 const eventsFromDashboard = [
     {
@@ -61,12 +62,21 @@ class EventDashboard extends Component {
         isOpen: false
     }
 
-    handleIsOpenToggle = ()  => {
-        this.setState(({isOpen}) => ({
+    handleIsOpenToggle = () => {
+        this.setState(({ isOpen }) => ({
             isOpen: !isOpen
         }))
 
-        
+
+    }
+
+    handleCreateEvent = newEvent => {
+        newEvent.id = cuid()
+        newEvent.hostPhotoURL = '/assets/user.png'
+        this.setState(({ events }) => ({
+            events: [...events, newEvent],
+            isOpen: false
+        }))
     }
 
     render() {
@@ -78,7 +88,7 @@ class EventDashboard extends Component {
                 </Grid.Column>
                 <Grid.Column width={6}>
                     <Button onClick={this.handleIsOpenToggle} positive content='Create Event' />
-                    {isOpen && <EventForm cancelFormOpen={this.handleIsOpenToggle}/>}
+                    {isOpen && <EventForm createEvent={this.handleCreateEvent} cancelFormOpen={this.handleIsOpenToggle} />}
 
                 </Grid.Column>
             </Grid>
